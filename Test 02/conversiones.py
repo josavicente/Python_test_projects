1ca89104541e999394e75de6f620b5b75973c1a2
Josa Vicente <jvicenpe@gmail.com>
2022-05-02 22:20:04 +0200

    v1.0

---
+++ Test 02/conversiones.py
@@ -0,0 +1,16 @@
+num1 = 20
+num2 = 30.5
+
+num1 = num1 + num2
+print(type(num1))
+print(type(num2))
+
+
+num1_1 = 5.8
+print(num1_1)
+print(type(num1_1))
+
+
+num2 = int(num1_1)
+print(num2)
+print(type(num2))
---
+++ Test 02/integers_floats.py
@@ -0,0 +1,9 @@
+mi_numero = 5.8
+print(mi_numero + 10)
+print(type(mi_numero))
+
+edad = input("Dime tu edad: ")
+print("Tu edad es :" + edad)
+
+nueva_edad = edad + 1
+print("Vas a cumplir: " + nueva_edad)
