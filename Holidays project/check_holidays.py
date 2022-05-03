1e60c3128edf6ccae5d53c3549ecb975ab24d8ef
Josa Vicente <jvicenpe@gmail.com>
2022-05-03 23:11:20 +0200

    v1.0

--- Holidays project/check_holidays.py
+++ Holidays project/check_holidays.py
@@ -1,13 +1,10 @@
-from __future__ import print_function
-
 import pandas as pd
 
-
 def load_excel_in_dataframe():
-    data = pd.read_excel('holidays.xlsx')
-    df = pd.DataFrame(data, index='index', columns=['Name', 'Date'])
+    data = pd.read_excel('/Users/josa/Documents/Developer/Python/Python Personal Projects/Holidays project/holidays.xlsx')
+    df = pd.DataFrame(data,  columns=['Name', 'Date'])
 
-    print(df)
+    print(df.iloc[0, 0])
 
 
 if __name__ == '__main__':
