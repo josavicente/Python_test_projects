4ff9d452424f86b8d2229a2d050a3880883e47fd
Josa Vicente <jvicenpe@gmail.com>
2022-05-04 20:51:43 +0200

    v1.0

--- Holidays project/check_holidays.py
+++ Holidays project/check_holidays.py
@@ -1,11 +1,18 @@
 import pandas as pd
+from gcsa.google_calendar import GoogleCalendar
+
+
 
 def load_excel_in_dataframe():
-    data = pd.read_excel('/Users/josa/Documents/Developer/Python/Python Personal Projects/Holidays project/holidays.xlsx')
-    df = pd.DataFrame(data,  columns=['Name', 'Date'])
+  #  data = pd.read_excel('/Users/josa/Documents/Developer/Python/Python Personal Projects/Holidays project/holidays.xlsx')
+   # df = pd.DataFrame(data,  columns=['Name', 'Date'])
 
-    print(df.iloc[0, 0])
+    # print(df.iloc[0, 0])
 
+    calendar = GoogleCalendar('jvicenpe@gmail.com')
+    for event in calendar:
+        print(event)
 
 if __name__ == '__main__':
     load_excel_in_dataframe()
+
