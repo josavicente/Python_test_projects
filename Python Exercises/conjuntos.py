d9d3e3da5267cd31eb4139326eabc81ae6d37cb9
Josa Vicente <jvicenpe@gmail.com>
2022-05-10 22:46:39 +0200

    v1.0

---
+++ Python Exercises/conjuntos.py
@@ -0,0 +1,19 @@
+conjunto = set()
+conjunto = {1,2,3,4,5,6,7,8,9}
+
+conjunto.add(0)
+
+print(conjunto)
+
+grupo = { 'josa', 'joao', 'Nico'}
+
+print ('josa' in grupo)
+
+pila = [3,4,5]
+pila.append(6)
+pila.append(7)
+
+print(pila)
+
+print(pila.pop())
+print(pila)
