7eea3980b3a90587a5b26682a5bd3c09d307c0d2
Josa Vicente <jvicenpe@gmail.com>
2022-05-16 21:24:19 +0200

    v1.0

--- Python Exercises/funciones.py
+++ Python Exercises/funciones.py
@@ -23,3 +23,21 @@ def suma(a,b):
 s = suma(2,4)
 
 print(s)
+
+def doblar_valores(numeros):
+    for i,n in enumerate(numeros):
+        numeros[i] *= 2
+
+ns = [10,20,30,40,50]
+doblar_valores(ns)
+print(ns)
+
+def doblar_numero(numero):
+    return numero * 2
+n = 10
+n = doblar_numero(n)
+print(n)
+
+ns = [10,20,30,40,50]
+print(doblar_valores(ns[:]))
+print(ns)
