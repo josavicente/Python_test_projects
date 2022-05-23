f44ed0a291e1e2bd86ec121c50648d255c036275
Josa Vicente <jvicenpe@gmail.com>
2022-05-23 22:51:51 +0200

    v1.0

--- Python Exercises/herencia.py
+++ Python Exercises/herencia.py
@@ -2,13 +2,57 @@ import py_compile
 
 
 class Producto:
-    def __init__(self, referencia, tipo, nombre, pvp, descripcion, productor=None, distribuidor=None, isbn=None, autor=None):
+    def __init__(self, referencia, nombre, pvp, descripcion):
         self.referencia = referencia
-        self.tipo = tipo
         self.nombre = nombre
         self.pvp = pvp
         self.descripcion = descripcion
-        self.productor = productor
-        self.distribuidor = distribuidor
-        self.isbn = isbn
-        self.autor = autor        
+    def __str__(self) -> str:
+        return """
+        REFENCIA \t{}
+        NOMBRE \t\t{}
+        PVP \t\t{}
+        DESCRIPCION \t{}""".format(self.referencia, self.nombre, self.pvp, self.descripcion)
+
+class Adorno(Producto):
+    pass
+
+class Alimento(Producto):
+    productor = ""
+    distribuidor = ""
+    
+    def __str__(self) -> str:
+        return """
+        REFENCIA \t{}
+        NOMBRE \t\t{}
+        PVP \t\t{}
+        DESCRIPCION \t{}
+        PRODUCTOR \t{}
+        DISTRIBUIDOR \t{}""".format(self.referencia, self.nombre, self.pvp, self.descripcion, self.productor, self.distribuidor)
+    
+class Libro(Producto):
+    autor = ""
+    isbn = ""
+    
+    def __str__(self) -> str:
+        return """
+        REFENCIA \t{}
+        NOMBRE \t\t{}
+        PVP \t\t{}
+        DESCRIPCION \t{}
+        AUTOR \t\t{}
+        ISBN \t\t{}""".format(self.referencia, self.nombre, self.pvp, self.descripcion, self.autor, self.isbn)
+    
+a = Adorno(2034,"Vaso adornado", 15, "Vaso adornado")
+print(a)
+
+al = Alimento(2035,"Botella", 10, "Botella")
+al.productor = "Productor"
+al.distribuidor = "Distribuidor"
+print(al)
+
+li = Libro(2036,"Libro", 10, "Libro")
+li.autor = "Autor"
+li.isbn = 2141234512342
+
+print(li)
