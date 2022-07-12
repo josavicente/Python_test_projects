d72f78ba7a8841acf1994913b28e9a91ee28d5da
Josa Vicente <jvicenpe@gmail.com>
2022-07-12 23:05:25 +0200

    v1.0

---
+++ Python Exercises/Advance modules/aleatorio.py
@@ -0,0 +1,8 @@
+import random
+
+print(random.random())
+
+l = [1,2,3,4,5,6,7,8,9,10]
+
+l = random.shuffle(l)
+print(l)
---
+++ Python Exercises/Advance modules/mates.py
@@ -0,0 +1,7 @@
+import math
+
+pi = 3.14159
+
+print(round(pi))
+print(math.floor(pi))
+print(math.ceil(pi))
