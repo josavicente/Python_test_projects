ddf00d82b57559270d611dceb466dfa0b91c4e5d
Josa Vicente <jvicenpe@gmail.com>
2022-07-12 22:48:14 +0200

    v1.0

--- Python Exercises/Advance modules/fechas.py
+++ Python Exercises/Advance modules/fechas.py
@@ -2,6 +2,18 @@ import datetime
 
 dt = datetime.datetime.now()
 
+# Printing the time in the format of hour:minute:second.
 print("{}:{}:{}".format(dt.hour, dt.minute, dt.second))
 
+# Printing the date in the format of day/month/year.
 print("{}/{}/{}".format(dt.day, dt.month, dt.year))
+
+
+# Importing the locale module.
+import locale
+
+# Setting the locale to Spanish.
+# locale.setlocale(locale.LC_ALL, 'es-ES')
+
+import pytz
+print(pytz.all_timezones)
